a363cd7c46daedf5bdef9737c7ab9d7baf7b5748
Jacques HU <jacqueshu@Apple-MacBook-Pro-14-M4-de-Diplay.local>
2024-11-20 10:45:01 +0100

    initial commit

---
+++ GameMix/GameMixApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameMixApp.swift
+//  GameMix
+//
+//  Created by Jacques HU on 20/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct GameMixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
