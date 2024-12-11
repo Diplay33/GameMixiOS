123d2b452bfb41db95445d0edbbd40b01252b52d
Jacques HU <jacqueshu@Apple-MacBook-Pro-14-M4-de-Diplay.local>
2024-12-11 10:13:21 +0100

    feat(app): add background component

---
+++ GameMix/EntryView.swift
@@ -0,0 +1,20 @@
+//
+//  EntryView.swift
+//  GameMix
+//
+//  Created by Jacques HU on 11/12/2024.
+//
+
+import SwiftUI
+
+struct EntryView: View {
+    var body: some View {
+        AppBackground {
+            Text("Hello, world!")
+        }
+    }
+}
+
+#Preview {
+    EntryView()
+}
---
+++ GameMix/View/Helper/AppBackground.swift
@@ -0,0 +1,28 @@
+//
+//  AppBackground.swift
+//  GameMix
+//
+//  Created by Jacques HU on 11/12/2024.
+//
+
+import SwiftUI
+
+struct AppBackground<Content: View>: View {
+    var contentView: () -> Content
+    
+    init(@ViewBuilder contentView: @escaping () -> Content) {
+        self.contentView = contentView
+    }
+    
+    var body: some View {
+        contentView()
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.background)
+    }
+}
+
+#Preview {
+    AppBackground {
+        Text("Hello, World!")
+    }
+}
