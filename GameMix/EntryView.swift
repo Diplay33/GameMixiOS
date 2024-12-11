fb5473de472a8060d5b51ff3af634dcc201a3e24
Jacques HU <jacqueshu@Apple-MacBook-Pro-14-M4-de-Diplay.local>
2024-12-11 11:29:40 +0100

    feat(home_view): play button leads to an another view

--- GameMix/EntryView.swift
+++ GameMix/EntryView.swift
@@ -9,29 +9,31 @@ import SwiftUI
 
 struct EntryView: View {
     var body: some View {
-        AppBackground {
-            VStack {
-                Spacer()
-                
-                Image("logoCompleteTransparent")
-                
-                Spacer()
-                
-                Button(action: {}) {
-                    ZStack {
-                        Rectangle()
-                            .cornerRadius(20)
-                            .frame(height: 60)
-                            .frame(maxWidth: .infinity)
-                            .padding(.horizontal)
-                        
-                        Text("Jouer")
-                            .font(.system(size: 32, weight: .bold, design: .rounded))
-                            .foregroundStyle(Color.lightLabel)
+        NavigationStack {
+            AppBackground {
+                VStack {
+                    Spacer()
+                    
+                    Image("logoCompleteTransparent")
+                    
+                    Spacer()
+                    
+                    NavigationLink(destination: Text("Hello, World!")) {
+                        ZStack {
+                            Rectangle()
+                                .cornerRadius(20)
+                                .frame(height: 60)
+                                .frame(maxWidth: .infinity)
+                                .padding(.horizontal)
+                            
+                            Text("Jouer")
+                                .font(.system(size: 32, weight: .bold, design: .rounded))
+                                .foregroundStyle(Color.lightLabel)
+                        }
                     }
+                    
+                    Spacer()
                 }
-                
-                Spacer()
             }
         }
     }
